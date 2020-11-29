8a54a87079cda81282a56b398914e0ebf3da9816
lilas <lilasli@hotmail.fr>
2020-11-29 20:50:10 +0100

    Finalisation de l'application

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,9 +4,23 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Main class of the project to start the programm and show the interface Park'It to the console.
+ *
+ * @author lilas
+ */
 public class App {
+    /**
+     * @param logger logger
+     */
     private static final Logger logger = LogManager.getLogger("App");
-    public static void main(String args[]){
+
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(String[] args) {
         logger.info("Initializing Parking System");
         InteractiveShell.loadInterface();
     }
