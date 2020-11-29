db84981b31004a5903b8c701b99f030fac1dd37c
lilas <lilasli@hotmail.fr>
2020-11-29 19:17:21 +0100

    29/11/2020

--- src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOIT.java
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOIT.java
@@ -6,17 +6,31 @@ import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-class ParkingSpotDAOIT{
-   private static DataBasePrepareService dataBasePrepareService;
-   private static ParkingSpotDAO parkingSpotDAO;
+/**
+ * The type Parking spot DAOIT.
+ * @author lilas
+ */
+class ParkingSpotDAOIT {
+    private static DataBasePrepareService dataBasePrepareService;
+    private static ParkingSpotDAO parkingSpotDAO;
 
+    @AfterAll
+    private static void tearDown() {
+        dataBasePrepareService.clearDataBaseEntries();
+    }
 
+    /**
+     * Sets up per test.
+     *
+     * @throws Exception the exception
+     */
     @BeforeEach
     void setUpPerTest() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
@@ -24,61 +38,176 @@ class ParkingSpotDAOIT{
         parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
         dataBasePrepareService.clearDataBaseEntries();
     }
-    @AfterAll
-    private static void tearDown(){
-        dataBasePrepareService.clearDataBaseEntries();
-    }
+
+    /**
+     * Gets next available car slot test.
+     */
+    @DisplayName( "Test to verify the nextAvailable spot with a car" )
     @Test
-    void getNextAvailableCarSlotTest() {
+    void getNextAvailableCarSpotTest() {
+        /*
+        Given a car park in spot 1
+         */
         ParkingType car = ParkingType.CAR;
-        ParkingSpot parkingSpot = new ParkingSpot(1,car,false);
-       parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
+        ParkingSpot parkingSpot = new ParkingSpot(1, car, false);
+        /*
+        When the parking is update
+         */
         parkingSpotDAO.updateParking(parkingSpot);
-        assertEquals(2,parkingSpotDAO.getNextAvailableSlot(car));
+          /*
+        Then verify the first parking spot available for a car is the spot 2
+         */
+        assertEquals(2, parkingSpotDAO.getNextAvailableSlot(car));
     }
-    @Test
-    void getNextAvailableCarSlotEmptyDataTest() {
 
-        assertEquals(1,parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
-    }
+    /**
+     * Gets next available car slot empty data test.
+     */
+    @DisplayName( "Test to verify the initial next available spot with empty data with a car" )
     @Test
-    void getNextAvailableBikeSlotEmptyDataTest() {
+    void getNextAvailableCarSpotEmptyDataTest() {
+         /*
+        Given
+         */
+        /*
+        When
+         */
+        /*
+        Then verify the first parking spot available for a car is the spot 1
+         */
+        assertEquals(1, parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
+    }
 
-        assertEquals(4,parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE));
-    }
+    /**
+     * Gets next available bike slot empty data test.
+     */
+    @DisplayName( "Test to verify the initial next available bike slot with empty data" )
     @Test
-    void getNextAvailableBikeSlotTest() {
+    void getNextAvailableBikeSpotEmptyDataTest() {
+        /*
+        Given
+         */
+        /*
+        When
+         */
+        /*
+        Then verify the first parking spot available for a bike is the spot 4
+         */
+        assertEquals(4, parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE));
+    }
+
+    /**
+     * Gets next available bike slot test.
+     */
+    @DisplayName( "Test to verify the nextAvailable slot with a bike" )
+    @Test
+    void getNextAvailableBikeSpotTest() {
+        /*
+        Given a bike park in spot 4
+         */
         ParkingType bike = ParkingType.BIKE;
-        ParkingSpot parkingSpot = new ParkingSpot(4,bike,false);
-       parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
+        ParkingSpot parkingSpot = new ParkingSpot(4, bike, false);
+        /*
+        When the parking is update
+         */
         parkingSpotDAO.updateParking(parkingSpot);
-        assertEquals(5,parkingSpotDAO.getNextAvailableSlot(bike));
+         /*
+        Then verify the first parking spot available for a bike is the spot 5
+         */
+        assertEquals(5, parkingSpotDAO.getNextAvailableSlot(bike));
     }
+
+    /**
+     * Gets next available bike slot error test.
+     */
+    @DisplayName( "Test to Gets next available bike slot error test." )
     @Test
-    void getNextAvailableBikeSlotErrorTest() {
+    void getNextAvailableBikeSpotErrorTest() {
+         /*
+        Given a bike park full
+         */
         ParkingType bike = ParkingType.BIKE;
-        ParkingSpot parkingSpot = new ParkingSpot(4,bike,false);
-        ParkingSpot parkingSpot1 = new ParkingSpot(5,bike,false);
-        parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
+        ParkingSpot parkingSpot = new ParkingSpot(4, bike, false);
+        ParkingSpot parkingSpot1 = new ParkingSpot(5, bike, false);
+        /*
+        When the parking is update
+         */
         parkingSpotDAO.updateParking(parkingSpot);
         parkingSpotDAO.updateParking(parkingSpot1);
+         /*
+        Then verify no parkingspot available is returned
+         */
         assertEquals(0, parkingSpotDAO.getNextAvailableSlot(bike));
 
     }
+
+    /**
+     * Gets next available car spot error test.
+     */
+    @DisplayName( "Test to get next available car spot error test" )
+    @Test
+    void getNextAvailableCarSlotErrorTest() {
+        /*
+        Given a car park full
+         */
+        ParkingType car = ParkingType.CAR;
+        ParkingSpot parkingSpot = new ParkingSpot(1, car, false);
+        ParkingSpot parkingSpot1 = new ParkingSpot(2, car, false);
+        ParkingSpot parkingSpot2 = new ParkingSpot(3, car, false);
+         /*
+        When the parking is update
+         */
+        parkingSpotDAO.updateParking(parkingSpot);
+        parkingSpotDAO.updateParking(parkingSpot1);
+        parkingSpotDAO.updateParking(parkingSpot2);
+        /*
+        Then verify no parkingspot available is returned
+         */
+        assertEquals(0, parkingSpotDAO.getNextAvailableSlot(car));
+
+    }
+
+    /**
+     * Update parking car test.
+     */
+    @DisplayName( " Update parking car test." )
     @Test
     void updateParkingCarTest() {
+        /*
+        Given a parkingSpot
+         */
         ParkingType car = ParkingType.CAR;
-        ParkingSpot parkingSpot = new ParkingSpot(1,car,false);
-        boolean result =  parkingSpotDAO.updateParking(parkingSpot);
+        ParkingSpot parkingSpot = new ParkingSpot(1, car, false);
+        /*
+        When the parking is update
+         */
+        boolean result = parkingSpotDAO.updateParking(parkingSpot);
+        /*
+        Then verify is update and next available parking spot is 2
+         */
         assertTrue(result);
-        assertEquals(2,parkingSpotDAO.getNextAvailableSlot(car));
+        assertEquals(2, parkingSpotDAO.getNextAvailableSlot(car));
     }
+
+    /**
+     * Update parking bike test.
+     */
+    @DisplayName( "Update parking bike test" )
     @Test
     void updateParkingBikeTest() {
+        /*
+        Given a parkingSpot
+         */
         ParkingType bike = ParkingType.BIKE;
-        ParkingSpot parkingSpot = new ParkingSpot(4,bike,false);
-        boolean result =  parkingSpotDAO.updateParking(parkingSpot);
+        ParkingSpot parkingSpot = new ParkingSpot(4, bike, false);
+        /*
+        When the parking is update
+         */
+        boolean result = parkingSpotDAO.updateParking(parkingSpot);
+        /*
+        Then verify is update and next available parking spot is 2
+         */
         assertTrue(result);
-        assertEquals(5,parkingSpotDAO.getNextAvailableSlot(bike));
+        assertEquals(5, parkingSpotDAO.getNextAvailableSlot(bike));
     }
 }
