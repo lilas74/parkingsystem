b16ae09f4cec83330a2e6c0bc8d7a7ead1ffc405
lilas <lilasli@hotmail.fr>
2020-12-01 17:00:50 +0100

    Version operationnelle

--- src/test/java/com/parkit/parkingsystem/unitaires/constants/FareTest.java
+++ src/test/java/com/parkit/parkingsystem/unitaires/constants/FareTest.java
@@ -1,11 +1,22 @@
 package com.parkit.parkingsystem.unitaires.constants;
 
+import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.unitaires.service.FareCalculatorServiceTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Field;
+
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FareTest extends FareCalculatorServiceTest {
 
@@ -38,6 +49,26 @@ public class FareTest extends FareCalculatorServiceTest {
     @DisplayName( "Calculate Free Fare for a car " )
     public void VerifyFreeFareAreSetCorretlyForACarTest(int arg) {
         double result = arg * Fare.FREE_CAR_RATE_PER_HOUR;
+
         Assertions.assertEquals(0, result);
     }
+    @Test
+    public void verifyFareConstantArePublicStaticFinalTest() throws Exception{
+        Class cls = Fare.class;
+        Field field = cls.getField("CAR_RATE_PER_HOUR");
+        Field field1 = cls.getField("BIKE_RATE_PER_HOUR");
+        Field field2= cls.getField("FREE_CAR_RATE_PER_HOUR");
+        Field field3 = cls.getField("FREE_CAR_RATE_PER_HOUR");
+
+        int modifiers = field.getModifiers();
+        int modifiers1 = field1.getModifiers();
+        int modifiers2= field2.getModifiers();
+        int modifiers3 = field3.getModifiers();
+
+        assertTrue(Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers));
+        assertTrue(Modifier.isPublic(modifiers1) && Modifier.isStatic(modifiers1) && Modifier.isFinal(modifiers1));
+        assertTrue(Modifier.isPublic(modifiers2) && Modifier.isStatic(modifiers2) && Modifier.isFinal(modifiers2));
+        assertTrue(Modifier.isPublic(modifiers3) && Modifier.isStatic(modifiers3) && Modifier.isFinal(modifiers3));
+    }
+
 }
