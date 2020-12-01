794ced3727db6a4026278b10c38df258d8864912
lilas <lilasli@hotmail.fr>
2020-12-01 09:34:18 +0100

    commit 01/12/2020

--- src/test/java/com/parkit/parkingsystem/unitaires/service/InteractiveShellTest.java
+++ src/test/java/com/parkit/parkingsystem/unitaires/service/InteractiveShellTest.java
@@ -1,4 +1,4 @@
-package com.parkit.parkingsystem.unitaires.util;
+package com.parkit.parkingsystem.unitaires.service;
 import com.parkit.parkingsystem.service.InteractiveShell;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,21 +7,26 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
  * The type Interactive shell test.
  */
-public class InteractiveShellTest {
+public class InteractiveShellTest  {
     /**
      * The Interactive shell.
      */
     InteractiveShell interactiveShell ;
+    InputStream inputStream ;
+    OutputStream outputStream ;
 
     @BeforeEach
     private void setUpPerTest() {
+
         interactiveShell = new InteractiveShell();
     }
 
@@ -32,24 +37,23 @@ public class InteractiveShellTest {
      */
     @Test
         @DisplayName("tests if the right message is displayed when enteing the app and leaving it right away")
-        public void loadInterfaceAnsWer3Test() throws IOException {
+        public void loadInterfaceAnsWerTest() throws Exception {
             //ARRANGE
             ByteArrayOutputStream outContent = new ByteArrayOutputStream();
             System.setOut(new PrintStream(outContent));
 
 
             String initialString = "3\n";
-            InputStream inputStream = new ByteArrayInputStream(initialString.getBytes());
+            inputStream = new ByteArrayInputStream(initialString.getBytes());
             System.setIn(inputStream);
 
-            InteractiveShell.loadInterface();
+            interactiveShell.loadInterface();
 
             String out = outContent.toString();
 
 
-            String load = "Please select an option. Simply enter the number to choose an action";
-
-            Assert.assertThat(out, containsString("3 Shutdown System"));
+            assertThat(out, containsString("3 Shutdown System"));
+            inputStream.close();
         }
 
 }
